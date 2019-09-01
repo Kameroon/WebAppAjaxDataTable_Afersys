4eec40e75afaeebe74ceddb2fc9d515c53021a70
DAVID MIKE <aidipifr@hotmail.com>
2019-09-02 00:29:45 +0200

    Mon prémier commit

---
+++ WebAppAjaxDataTable/Controllers/EmployeeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppAjaxDataTable.Models;
+using WebAppAjaxDataTable.Repositories;
+
+namespace WebAppAjaxDataTable.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee  
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private EmployeeRepository EmployeeRepository;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetList()
+        {
+            EmployeeRepository = new EmployeeRepository();
+            var employeeList = EmployeeRepository.PopulateEmployeeList();
+
+            return Json(new { data = employeeList }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AddOrEdit(int id)
+        {
+            if (id == 0)
+            {
+                return View(new Employee());
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
